aeafa44948aa5cbfdf2ef36018fbe439b62ecc39
Kerri Sierra-Scott <kerriss66@hotmail.com>
2020-01-16 15:02:03 -0600

    fixed search error

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -10,6 +10,7 @@ namespace TechJobs.Controllers
         {
             ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
+            ViewBag.error = "";
             return View();
         }
 
@@ -18,19 +19,26 @@ namespace TechJobs.Controllers
         public IActionResult Results(string searchType, string searchTerm)
         {
             ViewBag.columns = ListController.columnChoices;
-
             List<Dictionary<string, string>> jobs;
-            if (searchType.Equals("all"))
-            {
-                jobs = JobData.FindByValue(searchTerm);
-            }
-            else
-            {
-                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
-            }
 
-            ViewBag.jobs = jobs;
-            
+            if (searchTerm == null)
+            {
+                ViewBag.error = "Please enter a search term";
+            } else
+            {
+                if (searchType.Equals("all"))
+                {
+                    jobs = JobData.FindByValue(searchTerm);
+                }
+                else
+                {
+                    jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                }
+
+                ViewBag.jobs = jobs;
+
+            }
+          
             return View("Index");
         }
 
